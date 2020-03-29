1dee0c875e066c4e4409e6633faaf074b2f0fa7a
pedro.cabral-at-407961336480 <pedro.cabral@napista.com.br>
2020-03-28 23:29:52 -0300

    Initial commit

---
+++ src/backoffice/controllers/customer.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Post, Body, Put, Delete, Param } from "@nestjs/common";
+import { Customer } from "../models/customer.model";
+import { Result } from "../models/result.model";
+
+@Controller('v1/customers')
+export class CustomerController {
+
+    @Get()
+    get() {
+        return new Result(null, true, [], null);
+    }
+
+    @Get(':document')
+    getById(@Param('document') document) {
+        return new Result(null, true, {}, null);
+    }
+
+    @Post()
+    post(@Body() body: Customer) {
+        return new Result('Cliente criado com sucesso!', true, body, null);
+    }
+
+    @Put(':document')
+    put(@Body() body, @Param('document') document) {
+        return new Result('Cliente alterado com sucesso!', true, body, null);
+    }
+
+    @Delete(':document')
+    delete(@Param('document') document) {
+        return new Result('Cliente removido com sucesso!', true, null, null);
+    }
+
+}
---
+++ src/backoffice/models/customer.model.ts
@@ -0,0 +1,13 @@
+export class Customer {
+
+    constructor(
+        public name: string,
+        public documente: string,
+        public email: string,
+        public password: string,
+        public active: boolean
+    ) {
+        
+    }
+
+}
